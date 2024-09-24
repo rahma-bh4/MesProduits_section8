0ffe5707dc1397739607e8c8ce3b34dd9e7fdf91
rahma-bh4 <benhassine.rahma4@gmail.com>
2024-09-24 01:36:42 +0100

    First commit

---
+++ src/app/produits/produits.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produits',
+  templateUrl: './produits.component.html'
+  
+})
+export class ProduitsComponent  {
+    produits: string[];
+    
+    constructor() { 
+      this.produits = ["PC Asus", "Imprimante Epson", "Tablette Samsung"]; 
+     } 
+    
+}
